83c6f5589b265cc0c80989e1d293dce3cd436f01
Timyr <manyakhin.28@mail.ru>
2021-11-11 11:10:50 +0300

    5

---
+++ other.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace;
+
+int main()
+{
+
+
+    int a = 10;
+    cout << "Hello world!" << endl;
+    for (int i = 0; i < a; i++)
+    {
+        cout << "commit" << a << endl;
+    }
+
+
+
+}
